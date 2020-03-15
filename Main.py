52a8bdcece3d4ec87cfae2e76a02561cdf306565
Nicolas-le <nicolas.ruth@web.de>
2020-03-15 14:42:01 +0100

    new path, independent of local machine

--- Main.py
+++ Main.py
@@ -6,8 +6,11 @@
 import os
 import textProcessing
 
-dataPath = "/home/ocin/Dokumente/Studium/Leipzig/Semester5/wissensgeschichte/hausarbeit/python/TextClusteringFrauenwahlrecht/korpus/"
+dataPath = "korpus/"
 texts = textProcessing.getAllTexts(dataPath)
+
+for t in texts:
+    print(t.author)
 
 
 
