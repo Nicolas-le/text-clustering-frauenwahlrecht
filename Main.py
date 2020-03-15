00a58dd47c20575bac6afb3e444633631d65a69e
Nicolas-le <nicolas.ruth@web.de>
2020-03-15 15:37:14 +0100

    first prints and test

--- Main.py
+++ Main.py
@@ -10,7 +10,14 @@ import tdIdf
 dataPath = "korpus/"
 documents = textProcessing.getAllTexts(dataPath)
 
+#documents = {filename: object with extracted info}
+
 td_Idf = tdIdf.getTFIDF(documents)
+
+for file in documents:
+    print(file)
+    print(td_Idf[file]["ist"])
+    #add function if word isn't in file
 
 
 
