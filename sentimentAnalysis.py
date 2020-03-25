64af07d6a52c7d1bcba6bae81b23ea7d32114cd0
Nicolas-le <nicolas.ruth@web.de>
2020-03-25 16:07:11 +0100

    completed a main function which is able to perform a sentiment analysis. if the source is a list of cooccs, the the helper function list to string is called

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -1,7 +1,32 @@
 from textblob_de import TextBlobDE as tb
 
-def sentAnalysis(coOccurrences):
+def sentAnalysis(source, isCoOc):
+    """
+    Polarity is float which lies in the range of [-1,1] where 1 means positive statement and -1
+    means a negative statement. Subjective sentences generally refer to personal opinion,
+    emotion or judgment whereas objective refers to factual information. Subjectivity is
+    also a float which lies in the range of [0,1].
 
-    blob = tb(coOccurrences)
+    :param source: This is the source for which the sentiment should be calculated
+    :param isCoOc: if True: The source is a list of already tokenized cooccurrences connected to some word
+                    if False: this already the whole text as a string
+    :return:       blob.sentiment as Sentiment(polarity=x, subjectivity=<0.3333333333333333>)
 
-    print(blob.sentiment)
+    """
+
+    if isCoOc:
+        blob = tb(listToString(source))
+    else:
+        blob = tb(source)
+
+    return blob.sentiment
+
+
+def listToString(coOccurrences):
+
+    string = ""
+    #textblob can only process strings, tokenization is done by itself
+    for coOc in coOccurrences:
+        string += " " + ' '.join(coOc)
+
+    return string
