027d33fbcdcaa8c216065534fecabb7cc47347ae
Nicolas-le <nicolas.ruth@web.de>
2020-03-29 15:21:08 +0200

    startet writing the vector creating function

--- featureExtraction/vectorAnalysis.py
+++ featureExtraction/vectorAnalysis.py
@@ -4,11 +4,28 @@
 
 """
 
-def createVektor():
-    print()
+def createVector(documents):
+
+    """vector:
+    sent text pol,
+    sent text subj,
+    sent cooc "Frauen" pol,
+    sent cooc Frauen subj,
+    td-idf "Frauen",
+    td-idf "Wahlrecht", (if none --> 0)
+    stilo word length
+    """
+    for file in documents:
+        filevector = (0,0,0,0,0,0,0)
+
+        sentText = sentimentAnalysis.sentAnalysis(documents[file].text,False)[0]
+
+
+
 
 def euclideanDistance():
     print()
 
 def createGraph():
     print()
+
